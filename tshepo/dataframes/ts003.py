bd466f5f3a1067027d603d738f07489a6c122a5b
erikvw <ew2789@gmail.com>
2016-04-21 14:42:19 +0200

    add lists

--- tshepo/dataframes/ts003.py
+++ tshepo/dataframes/ts003.py
@@ -5,6 +5,8 @@ import numpy as np
 from sqlalchemy import create_engine
 
 from bcpp_export.private_settings import LisCredentials
+from tshepo.modification_reasons import modification_reasons
+from tshepo.dose_status import dose_status
 
 engine = create_engine('mssql+pymssql://{user}:{passwd}@{host}:{port}/{db}'.format(
     user=LisCredentials.user, passwd=LisCredentials.password,
@@ -24,62 +26,10 @@ sql = 'select * from bhp007.dbo.ts003response as ts003 left join bhp007.dbo.TS00
 with engine.connect() as conn, conn.begin():
     df_data = pd.read_sql_query(sql, conn)
 
-dose_status = {
-    '1': 'Initial dose',
-    '2': 'Reduced or increased',
-    '3': 'Temporarily held',
-    '4': 'Resumed from a temporary hold',
-    '5': 'Discontinued',
-    '6': 'Resumed after Discontinuation'}
 df_dose_status = pd.DataFrame(dose_status.items(), columns=['code', 'status']).sort_values('code')
 
-reasons = {
-    '11': 'Initiation of Study medication ',
-    '12': 'Completion of Study follow-up',
-    '13': 'NVP scheduled modification',
-    '14': 'Virological failure',
-    '15': 'Toxicity related to other Study Medications',
-    '16': 'Recycling',
-    '50': 'Requires medication not permitted while taking study medications',
-    '51': 'Concomitant codition (TB,pregnancy)',
-    '52': 'Weight gain/loss',
-    '60': 'Concomitant codition resolved',
-    '70': 'Toxicity decreased/resolved',
-    '80': 'Clinician discretion',
-    '81': 'Patient request because of intolerance to study medication',
-    '84': 'Patient request for other reason',
-    '82': 'Non-compliance with clinic visits',
-    '83': 'Non-compliance with Study medications',
-    '20': 'Anemia',
-    '21': 'Bleeding',
-    '23': 'Diarrhea',
-    '24': 'Fatigue',
-    '25': 'Fever',
-    '38': 'Glucose intolerance/diabetes',
-    '26': 'Headache',
-    '36': 'Hypertriglyceridemia',
-    '37': 'Hypercholesterolemia',
-    '27': 'Hepatotoxicity (not lactic acidosis)',
-    '42': 'Hypersensitivity reaction',
-    '33': 'Lactic acidosis',
-    '35': 'Myositis',
-    '28': 'Nausea',
-    '41': 'Nephrotoxicity',
-    '29': 'Neutropenia',
-    '22': 'Neuropsychiatric symptoms',
-    '39': 'Pancreatitis',
-    '34': 'Peripheral neuropathy',
-    '40': 'Rash',
-    '30': 'Thrombocytopenia',
-    '31': 'Vomiting',
-    '98': 'Other toxicity, specify',
-    '90': 'Death',
-    '0': 'Normal',
-    '1': 'Mild',
-    '2': 'Moderate',
-    '3': 'Severe',
-    '-3': 'Not applicable'}
-df_reasons = pd.DataFrame(reasons.items(), columns=['code', 'reason'])
+
+df_reasons = pd.DataFrame(modification_reasons.items(), columns=['code', 'reason'])
 
 df_data = pd.merge(df_data, df_dose_status, left_on='Q001A2', right_on='code', how='left')
 df_data = pd.merge(df_data, df_reasons, left_on='rx_modification_reason', right_on='code', how='left')
---
+++ tshepo/dose_status.py
@@ -0,0 +1,7 @@
+dose_status = {
+    '1': 'Initial dose',
+    '2': 'Reduced or increased',
+    '3': 'Temporarily held',
+    '4': 'Resumed from a temporary hold',
+    '5': 'Discontinued',
+    '6': 'Resumed after Discontinuation'}
